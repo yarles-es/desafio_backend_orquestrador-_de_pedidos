37549d962be7043bc4810ec3de8eae8ae9d65bcd
Yarles <yarles100@gmail.com>
2025-10-30 13:15:49 -0300

    feat: implementado configurações de recursos nest, variaveis ambientes e docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix(process.env.GLOBAL_PREFIX || '/api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
+
+  console.log(`Aplicação iniciada em: ${await app.getUrl()}`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Erro ao iniciar a aplicação:', error);
+
+  process.exit(1);
+});
